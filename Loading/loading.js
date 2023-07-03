dc58ae5c638d9b1146fbddda716366a2409af321
AHNDOORI <kaudo@msn.com>
2023-07-03 14:25:19 +0900

    Update loading.js

--- Loading/loading.js
+++ Loading/loading.js
@@ -1,6 +1,15 @@
 
 // width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes
 var unityInstance;
+createUnityInstance(document.querySelector("#unity-canvas"),{
+  dataUrl: "Build/InWebGL.data.unityweb",
+  frameworkUrl: "Build/InWebGL.framework.js.unityweb",
+  codeUrl: "Build/InWebGL.wasm.unityweb",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "DefaultCompany",
+  productName: "inappgame",
+  productVersion: "1.0",
+},onProgress).then(onComplete).catch((e)=>{onError(e);});
 
 function onError(exception){
   alert(exception);
@@ -28,13 +37,4 @@ window.onload = function(){;
   setTimeout(function(){
     window.dispatchEvent(new Event('resize'));
   },0);
-  createUnityInstance(document.querySelector("#unity-canvas"),{
-    dataUrl: "Build/InWebGL.data.unityweb",
-    frameworkUrl: "Build/InWebGL.framework.js.unityweb",
-    codeUrl: "Build/InWebGL.wasm.unityweb",
-    streamingAssetsUrl: "StreamingAssets",
-    companyName: "DefaultCompany",
-    productName: "inappgame",
-    productVersion: "1.0",
-  },onProgress).then(onComplete).catch((e)=>{onError(e);});
 }
