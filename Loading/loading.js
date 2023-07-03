11c8effb4f61afffac7c10abb21bed4e25735903
AHNDOORI <kaudo@msn.com>
2023-07-03 14:27:30 +0900

    Update loading.js

--- Loading/loading.js
+++ Loading/loading.js
@@ -1,15 +1,6 @@
 
 // width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes
 var unityInstance;
-createUnityInstance(document.querySelector("#unity-canvas"),{
-  dataUrl: "Build/InWebGL.data.unityweb",
-  frameworkUrl: "Build/InWebGL.framework.js.unityweb",
-  codeUrl: "Build/InWebGL.wasm.unityweb",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "DefaultCompany",
-  productName: "inappgame",
-  productVersion: "1.0",
-},onProgress).then(onComplete).catch((e)=>{onError(e);});
 
 function onError(exception){
   alert(exception);
@@ -37,4 +28,15 @@ window.onload = function(){;
   setTimeout(function(){
     window.dispatchEvent(new Event('resize'));
   },0);
+  createUnityInstance(document.querySelector("#unity-canvas"),{
+    dataUrl: "Build/InWebGL.data",
+    frameworkUrl: "Build/InWebGL.framework.js",
+    codeUrl: "Build/InWebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "inappgame",
+    productVersion: "1.0",
+    // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
+    // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
+  },onProgress).then(onComplete).catch((e)=>{onError(e);});
 }
