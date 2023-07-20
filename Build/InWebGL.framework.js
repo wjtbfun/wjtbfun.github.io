8c43ada59da8aff19bf42a801fbf1249ad1d29a9
BlueHamster <dlsgkr2856@gmail.com>
2023-07-20 17:11:06 +0900

    _
    _

--- Build/InWebGL.framework.js
+++ Build/InWebGL.framework.js
@@ -11091,7 +11091,7 @@ var ASM_CONSTS = {
       // full 4GB Wasm memories, the size will wrap back to 0 bytes in Wasm side
       // for any code that deals with heap sizes, which would require special
       // casing all heap size related code to treat 0 specially.
-      return 268435456;
+      return 536870912;
     }
 
 
