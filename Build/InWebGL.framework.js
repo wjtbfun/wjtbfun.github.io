fff5be17519d5e1a8f379da2bf5de980cbd52cec
BlueHamster <dlsgkr2856@gmail.com>
2023-07-27 16:37:30 +0900

    마스터변경추가
    마스터변경추가

--- Build/InWebGL.framework.js
+++ Build/InWebGL.framework.js
@@ -1976,13 +1976,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  2832372: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 2832427: function($0) {performance.now = function() { return $0; };},  
- 2832475: function($0) {performance.now = function() { return $0; };},  
- 2832523: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 2832578: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 2832639: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 2832703: function() {return Module.webglContextAttributes.powerPreference;}
+  2832484: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 2832539: function($0) {performance.now = function() { return $0; };},  
+ 2832587: function($0) {performance.now = function() { return $0; };},  
+ 2832635: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 2832690: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 2832751: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 2832815: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
