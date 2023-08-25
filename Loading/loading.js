ac15ffffcf5da44a33f63c697eaa63d6d276e9a8
BlueHamster <dlsgkr2856@gmail.com>
2023-08-25 16:40:43 +0900

    _
    _

--- Loading/loading.js
+++ Loading/loading.js
@@ -32,14 +32,14 @@ window.onload = function(){;
   setTimeout(function(){
     window.dispatchEvent(new Event('resize'));
   },0);
-      createUnityInstance(document.querySelector("#unity-canvas"), {
-        dataUrl: "Build/InWebGL.data",
-        frameworkUrl: "Build/InWebGL.framework.js",
-        codeUrl: "Build/InWebGL.wasm",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "WoonjingThinkbig",
-        productName: "Brainbattle",
-        productVersion: "1.0",
+  createUnityInstance(document.querySelector("#unity-canvas"), {
+    dataUrl: "Build/InWebGL.data.unityweb",
+    frameworkUrl: "Build/InWebGL.framework.js.unityweb",
+    codeUrl: "Build/InWebGL.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "WoonjingThinkbig",
+    productName: "Brainbattle",
+    productVersion: "1.0",
 /*
     dataUrl: "Build/InWebGL.data.unityweb",
     frameworkUrl: "Build/InWebGL.framework.js.unityweb",
